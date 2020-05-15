bd969aa8745c990eeb774c6ba4283df89dc0ed24
Soichiro Yoshimura <yoshimura@soichiro.org>
2020-05-16 02:05:58 +0900

    v1.0.0 implemented.

---
+++ README.md
@@ -0,0 +1,60 @@
+# Slack API command tool
+
+This is a simple tool to get access to [Slack API](https://api.slack.com/methods/) with shell command.
+This tool use only URL query parameter. This tool can't post big data.
+Only standard output is used.
+
+# How to use (with bash)
+
+```
+export SLACK_TOKEN=xoxb-9999999999999-9999999999999-xxxxxxxxxxxxxxxxxxxxxx
+npm run build
+node dist/index.js httpmethod apiname --args1 hogehoge --args2 fugafuga
+```
+
+# Example
+
+### Get users
+
+```
+node dist/index.js get users.list --limit 10
+```
+
+### Get users with cursor
+
+```
+export SLACK_COMMAND_AC_SLEEP=500
+node dist/index.js get users.list --cursor dXNlcjpVMDYxTkZUVDI= --limit 10
+```
+
+SLACK_COMMAND_AC_SLEEP is sleep time msec interval for continuous accesses.
+
+### Post message
+
+```
+node dist/index.js post chat.postMessage --channel CLFH073PU --text "Hello!"
+```
+
+# Auto cursor mode
+
+Auto cursor help to get all of responses in Slack with cursor.
+
+```
+node dist/index.js get users.list autocursor --limit 100
+```
+
+Output is Array JSON of response.data.
+
+# Use with [jq](https://stedolan.github.io/jq/) command
+
+### Fomat JSON
+
+```
+node dist/index.js get users.list --limit 10 | jq .
+```
+
+### Only ids
+
+```
+node dist/index.js get users.list --limit 10 | jq '.members[].id'
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,73 @@
-console.log('Hello TS');
+// Slack API methdos https://api.slack.com/methods
+
+import axios from 'axios';
+import minimist from 'minimist';
+import querystring from 'querystring';
+
+const SLACK_API_URL_BASE = 'https://slack.com/api/';
+
+if (!process.env.SLACK_TOKEN) {
+  console.log(
+    'Please set SLACK_TOKEN env parm. e.g. export SLACK_TOKEN=xoxb-9999999999999-9999999999999-xxxxxxxxxxxxxxxxxxxxxx'
+  );
+  process.exit(1);
+}
+const argv = minimist(process.argv.slice(2));
+
+const method = argv._[0];
+const apiname = argv._[1];
+const isAutocursor = argv._[2] === 'autocursor';
+
+delete argv._;
+
+const axiosInstance = axios.create({
+  baseURL: SLACK_API_URL_BASE,
+  timeout: 0,
+  headers: {
+    Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+});
+
+(async () => {
+  if (!isAutocursor) {
+    // Normal mode
+    const response = await (axiosInstance as any)[method](
+      apiname + '?' + querystring.stringify(argv)
+    );
+    if (response.status >= 300) {
+      console.error('Not OK status. response:');
+      console.error(response);
+    } else {
+      console.log(JSON.stringify(response.data));
+    }
+  } else {
+    // Auto Cursol mode (Use data?.response_metadata?.next_cursor and cursor param.)
+    const responses = [];
+    let cursor;
+    do {
+      const sleep = (msec: number) =>
+        new Promise((resolve) => setTimeout(resolve, msec));
+      if (process.env.SLACK_COMMAND_AC_SLEEP) {
+        await sleep(1000);
+      }
+
+      const currentRes = await (axiosInstance as any)[method](
+        apiname + '?' + querystring.stringify(argv)
+      );
+      if (currentRes.status >= 300) {
+        console.error('Not OK status. response:');
+        console.error(currentRes);
+        break;
+      }
+      responses.push(currentRes.data);
+      cursor = currentRes.data?.response_metadata?.next_cursor;
+      if (cursor) {
+        argv.cursor = cursor;
+      } else {
+        delete argv.cursor;
+      }
+    } while (cursor);
+    console.log(JSON.stringify(responses));
+  }
+})();
